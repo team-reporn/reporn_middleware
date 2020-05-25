915672ffac6c0e1fc3b648d4a0a50526ab065d24
emile <emile31.roch@gmail.com>
2020-05-25 10:17:17 +0200

    Create and Join Rooms

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,10 @@
+let roomsIds = ["elleBaise"];
+let s4 = () => {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+};
+
 module.exports = (io) => {
   io.on("connection", (socket) => {
     console.log("connection of a user");
@@ -19,12 +26,51 @@ module.exports = (io) => {
         // eslint-disable-next-line no-param-reassign
         socket.username = "host";
         socket.emit("created", roomId);
-        console.log("emitted create");
       } else {
         // eslint-disable-next-line no-param-reassign
         socket.username = "client";
         socket.emit("joined", roomId);
       }
+    });
+    socket.on("create room", () => {
+      console.log("creating a room");
+      let roomId = s4();
+      socket.leave(socket.room);
+      socket.join(roomId);
+      const numClients = io.sockets.adapter.rooms[roomId].length;
+
+      socket.emit("room info", { roomId: roomId, numClients: numClients });
+    });
+    socket.on("join a room", (roomId) => {
+      console.log("trying to join : ", roomId);
+      if (io.sockets.adapter.rooms[roomId]) {
+        socket.leave(socket.room);
+        socket.join(roomId);
+        const numClients = io.sockets.adapter.rooms[roomId].length;
+        let info = {
+          success: true,
+          roomInfo: {
+            roomId: roomId,
+            numClients: numClients,
+          },
+        };
+        console.log("info");
+        console.log(info);
+        socket.emit("joined", info);
+        io.to(roomId).emit("room info", {
+          roomId: roomId,
+          numClients: numClients,
+        });
+      } else {
+        console.log("No room found for this id");
+      }
+    });
+    socket.on("update room info", (roomId) => {
+      const numClients = io.sockets.adapter.rooms[roomId].length;
+      io.to(roomId).emit("room info", {
+        roomId: roomId,
+        numClients: numClients,
+      });
     });
     let readyCount = 0;
     socket.on("ready", () => {
